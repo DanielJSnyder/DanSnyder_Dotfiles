36be9876984ad07ee6f72a1d1a164b1966624282
Dan <snysly@umich.edu>
2016-04-30 22:28:38 -0400

    Partial updates to the setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,24 @@ ln -s $LOC/.bashrc .bashrc
 ln -s $LOC/.tmux.conf .tmux.conf
 ln -s $LOC/.vim .vim
 ln -s $LOC/.vimrc .vimrc
+
+#now setup and reclone all the other files
+echo "Setting up Vim"
+echo "cloning Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installing Plugins"
+vim +PluginInstall +qall
+
+echo "Installing YCM"
+cd ~/.vim/bundle/YouCompleteMe
+sudo apt-get install libclang-dev
+
+sudo apt-get install clang-3.5
+
+sudo apt-get install clang-3.5
+
+./install.py --clang-completer --system-libclang
+cd third_party/ycmd/
+cp /usr/lib/llvm-3.5/lib/libclang.so.1 .
+
